c885fe17b8cf53989435220797c5c7dec47bbdf3
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-01-27 13:36:45 +0300

    Create user model

---
+++ sub-checkerBot/core/models/user.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, declared_attr
+
+from .base import Base
+
+class User(Base):
+
+   user_id: Mapped[int] = mapped_column(primary_key=True)
+   subscription_end: Mapped[datetime | None] = mapped_column()
