9827efecbd4acc5529c684abd1d045b3e3219403
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-01-27 12:52:22 +0300

    Create main settings

---
+++ sub-checkerBot/core/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    token: str
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="SUBSCRIBE__",
+        env_file="../.env",
+    )
+
+    run: RunConfig
+
+settings = Settings()
