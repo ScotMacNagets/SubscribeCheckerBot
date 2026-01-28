a4fce0a41a9826af28cf9dc180dc14fece3f09b8
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-01-28 18:55:31 +0300

    Add background task for sub checking in main.py

--- sub-checkerBot/main.py
+++ sub-checkerBot/main.py
@@ -1,12 +1,15 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from core.config import settings
+from aiogram import Dispatcher
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.bot_instance import bot
 from core.models.db_helper import db_helper
 from middleware import DBMiddleware
+from handlers import subscription_router
+from services import subscription_checker
 
-bot = Bot(token=settings.run.token)
 dp = Dispatcher()
 
 dp.include_router(subscription_router)
@@ -15,6 +18,7 @@ dp.include_router(subscription_router)
 async def on_startup():
     await db_helper.create_tables()
     dp.update.middleware(DBMiddleware(db=db_helper))
+    # asyncio.create_task(subscription_checker(db=db_helper))
 
 @dp.shutdown()
 async def on_shutdown():
