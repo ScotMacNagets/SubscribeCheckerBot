8cc7f16171e109597bb474fc25e90e584c1403f0
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-01-31 16:14:24 +0300

    Add tariffs rates in config.py

--- sub-checkerBot/core/config.py
+++ sub-checkerBot/core/config.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class TariffConfig(BaseModel):
+    plan_1: str = "plan_1 месяц"
+    plan_3: str = "plan_3 месяца"
+    plan_6: str  = "plan_6 месяцев"
+
 class ChannelConfig(BaseModel):
     chan_id: int
 
@@ -24,3 +29,4 @@ class Settings(BaseSettings):
     channel: ChannelConfig
 
 settings = Settings()
+tariff = TariffConfig()
