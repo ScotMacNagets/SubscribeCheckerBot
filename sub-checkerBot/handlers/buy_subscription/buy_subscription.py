3f803ffd1c30e96d4dc7814f9e966fa4ca0f0930
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-01 21:37:09 +0300

    Add test payment to buy_subscription.py

--- sub-checkerBot/handlers/buy_subscription/buy_subscription.py
+++ sub-checkerBot/handlers/buy_subscription/buy_subscription.py
@@ -1,17 +1,21 @@
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, LabeledPrice, Message, PreCheckoutQuery
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from keyboards.payment_keyboard import build_payment_keyboard, confirm_pay, cancel_pay, back
+from core.config import settings
+from core.tariff import TARIFFS
+from keyboards.payment_keyboard import build_payment_keyboard
+from services.sub_add_and_check import add_or_update_subscription
 from .helpers import show_menu
 from .states import BuySubscription
-from keyboards.start_keyboard import buy_sub
+from keyboards.callback_text import start, payment, back
 
 router = Router()
 
 
 
-@router.callback_query(F.data == buy_sub)
+@router.callback_query(F.data == start.buy_sub)
 async def buy_sub_callback(query: CallbackQuery, state: FSMContext):
     await state.set_state(BuySubscription.choosing_tariff)
     await query.answer()
@@ -25,9 +29,10 @@ async def buy_sub_callback(query: CallbackQuery, state: FSMContext):
     F.data.startswith("plan_"),
 )
 async def tariff_callback(query: CallbackQuery, state: FSMContext):
-    tariff = query.data.split("_")[1]
+    tariff_id = query.data
+    tariff = TARIFFS[tariff_id].title
 
-    await state.update_data(tariff=tariff)
+    await state.update_data(tariff_id=tariff_id)
 
     await state.set_state(BuySubscription.confirming_payment)
 
@@ -41,22 +46,73 @@ async def tariff_callback(query: CallbackQuery, state: FSMContext):
 
 @router.callback_query(
     BuySubscription.confirming_payment,
-    F.data == confirm_pay,
+    F.data == payment.confirm_pay,
 )
 async def confirming_payment_callback(query: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    tariff = data["tariff"]
+    tariff_id = data["tariff_id"]
 
-    # Вставить логику оплаты и работы с бд
+    tariff = TARIFFS[tariff_id]
 
-    await query.answer()
-    await query.message.edit_text(
-        f"Оплата прошла успешно!\n Тариф на {tariff}"
+    prices = [
+        LabeledPrice(
+            label=f"Подписка {tariff.title}",
+            amount=tariff.price
+        )
+    ]
+
+    await query.message.answer_invoice(
+        title=f"Подписка",
+        description=tariff.title,
+        payload=tariff.payload,
+        provider_token=str(settings.payment.token),
+        currency="RUB",
+        prices=prices,
     )
 
+    await query.answer()
+
+@router.pre_checkout_query()
+async def process_pre_checkout_query(pre_checkout_query: PreCheckoutQuery):
+    await pre_checkout_query.answer(ok=True)
+
+@router.message(F.successful_payment)
+async def successful_payment(
+        message: Message,
+        session: AsyncSession,
+        state: FSMContext,
+):
+    user_id = message.from_user.id
+    payload = message.successful_payment.invoice_payload
+
+    tariff = next(
+        (
+            t for t in TARIFFS.values() if t.payload == payload
+        ),
+        None
+    )
+    print("Received payload:", message.successful_payment.invoice_payload)
+    print("Available tariffs:", [t.payload for t in TARIFFS.values()])
+    if tariff is None:
+        await message.answer("❌ Неизвестный тариф. Обратитесь к поддержке.")
+        return
+
+    new_end_date = await add_or_update_subscription(
+        session=session,
+        user_id=user_id,
+        days=tariff.days,
+    )
+
+    await message.answer(
+        f"✅ Оплата прошла успешно!\n"
+        f"Тариф: {tariff.title}\n"
+        f"Подписка активна до {new_end_date}"
+    )
     await state.clear()
 
-@router.callback_query((F.data == cancel_pay))
+
+
+@router.callback_query((F.data == payment.cancel_pay))
 async def to_main_menu(query: CallbackQuery, state: FSMContext):
     await state.clear()
     await query.answer()
@@ -66,7 +122,7 @@ async def to_main_menu(query: CallbackQuery, state: FSMContext):
     )
 
 
-@router.callback_query(F.data == back)
+@router.callback_query(F.data == back.back)
 async def go_back(query: CallbackQuery, state: FSMContext):
     current_state = await state.get_state()
 
