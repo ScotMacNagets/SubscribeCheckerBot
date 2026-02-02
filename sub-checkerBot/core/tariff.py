71171fc92b9dbb1669a4909b7032e19f4210166e
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-02 23:33:55 +0300

    Add new rows in tariff.py : hot and emoji

--- sub-checkerBot/core/tariff.py
+++ sub-checkerBot/core/tariff.py
@@ -7,29 +7,38 @@ class Tariff(BaseModel):
     days: int
     price: int
     payload: str
+    hot: bool = False
+    emoji: str
 
 
-TARIFFS = {
+TARIFFS: dict[str, Tariff]= {
     "plan_1": Tariff(
         id="plan_1",
         title="1 месяц",
         days=30,
         price=10000,
         payload="sub_1_month"
+,       hot=False,
+        emoji=""
+
     ),
     "plan_3": Tariff(
         id="plan_3",
         title="3 месяца",
         days=90,
         price=50000,
-        payload="sub_3_months"
+        payload="sub_3_months",
+        hot=False,
+        emoji=""
     ),
     "plan_6": Tariff(
         id="plan_6",
         title="6 месяцев",
         days=180,
         price=90000,
-        payload="sub_6_months"
+        payload="sub_6_months",
+        hot=True,
+        emoji="💎"
     )
 
 }
