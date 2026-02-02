8f8e5df31c538c53dc9e58fd674df7f9e616b3e6
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-02 17:13:35 +0300

    Create logger_config

---
+++ sub-checkerBot/core/logging_config.py
@@ -0,0 +1,9 @@
+# Настройка логирования
+import logging
+
+def configure_logging(level = logging.WARNING):
+    logging.basicConfig(
+        level=level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        format='[%(asctime)s.%(msecs)03d] %(module)-20s:%(lineno)-3d %(levelname)-7s - %(message)s',
+    )
