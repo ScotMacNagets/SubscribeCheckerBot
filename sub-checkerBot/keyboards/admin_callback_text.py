147930a7a835bc8c938f961384545adb40fd9815
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-05 21:22:23 +0300

    Create callback_text file for admin panel

---
+++ sub-checkerBot/keyboards/admin_callback_text.py
@@ -0,0 +1,19 @@
+class AdminMain:
+    OPEN = "admin_open"
+
+
+class AdminUsers:
+    HUMAN_RESOURCE = "admin_human_resource"
+    SEARCH_BY_USERNAME = "admin_users_search_id"
+    USER_DETAIL_PREFIX = "admin_user_"  # admin_user_<user_id>
+
+
+class AdminUserActions:
+    EXTEND_7 = "extend_7"
+    EXTEND_30 = "extend_30"
+    EXTEND_90 = "extend_90"
+    SET_END_DATE = "admin_user_set_end"
+    CANCEL_SUB = "admin_user_cancel_sub"
+    DELETE_USER = "admin_user_delete"
+    BACK_TO_ADMIN_MENU = "admin_back_main"
+
