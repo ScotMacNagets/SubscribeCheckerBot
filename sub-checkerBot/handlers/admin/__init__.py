5a723546bb36ef2eaa499c72fd542dad16d2b246
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-14 15:19:44 +0300

    Connect tariff.py router to the main.py

--- sub-checkerBot/handlers/admin/__init__.py
+++ sub-checkerBot/handlers/admin/__init__.py
@@ -2,10 +2,12 @@ from aiogram import Router
 
 from .users import router as users_router
 from .admin import router as main_router
+from .tariffs import router as tariffs_router
 
 admin_router = Router()
 admin_router.include_router(users_router)
 admin_router.include_router(main_router)
+admin_router.include_router(tariffs_router)
 
 __all__ = ("admin_router",)
 
---
+++ sub-checkerBot/handlers/admin/tariffs/__init__.py
@@ -0,0 +1,5 @@
+from .tariffs import router
+
+__all__ = (
+    "router"
+)
