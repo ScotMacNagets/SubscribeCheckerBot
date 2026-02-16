f1e7c599e976f58b1526df4287396851559bd0b4
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-16 13:15:07 +0300

    Move admin text to constants.py

--- sub-checkerBot/core/constants.py
+++ sub-checkerBot/core/constants.py
@@ -1,3 +1,7 @@
 
 
 TARIFF_CALLBACK_PREFIX="sub_"
+
+class CommandText:
+    ADMIN_MENU="admin_menu"
+    CREATE_TARIFF="create_tariff"
--- sub-checkerBot/handlers/admin/admin.py
+++ sub-checkerBot/handlers/admin/admin.py
@@ -1,13 +1,16 @@
 from aiogram import Router
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from core.constants import CommandText
 from core.text import AdminMenu
+from handlers.admin.tariffs.fsm_states import CreateTariff
 from keyboards.admin_main_menu import build_main_menu
 
 router = Router()
 
-@router.message(Command("admin_users"))
+@router.message(Command(CommandText.ADMIN_MENU))
 async def open_admin_menu(message: Message):
 
     await message.answer(
