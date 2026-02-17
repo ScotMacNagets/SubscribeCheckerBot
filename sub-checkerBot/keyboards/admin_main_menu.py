6cc0f64a6d11b62faaf456629b2a8f1c21d13fcb
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-17 18:17:31 +0300

    Update admin menu main keyboard: replace text with callbacks and add new button witch displays only for superuser

--- sub-checkerBot/keyboards/admin_main_menu.py
+++ sub-checkerBot/keyboards/admin_main_menu.py
@@ -1,19 +1,30 @@
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from callbacks.admin_callback_text import AdminUsers, AdminTariffs
+from callbacks.admin_callback_text import AdminUsers, AdminTariffs, AdminTariffsActions
+from core.config import settings
+from core.text import AdminTariffKeyboard, AdminTariffMenu, AdminUsersMenu
 
 
-def build_main_menu() -> InlineKeyboardMarkup:
+def build_main_menu(username: str) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
 
     builder.button(
-        text="Управление пользователями",
+        text=AdminUsersMenu.USER_MANAGE_MENU,
         callback_data=AdminUsers.HUMAN_RESOURCE,
     )
+
     builder.button(
-        text="Управление тарифами",
+        text=AdminTariffMenu.TARIFF_MENU,
         callback_data=AdminTariffs.TARIFFS_MENU,
     )
+
+    if username == settings.admin.super_user:
+        builder.button(
+            text=AdminTariffKeyboard.START_CREATING,
+            callback_data=AdminTariffsActions.START_CREATING
+        )
+
+    builder.adjust(1)
 
     return builder.as_markup()
