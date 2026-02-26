b8f9ee6d7df9f2ce0b76ac9f398136ca88821ec5
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-26 12:23:03 +0300

    Import datetime and Subscriptions in helpers.py

--- sub-checkerBot/handlers/admin/users/helpers.py
+++ sub-checkerBot/handlers/admin/users/helpers.py
@@ -1,6 +1,9 @@
-from datetime import date
+from datetime import date, datetime
 
-from core.models import User
+from sqlalchemy import select, and_
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import User, Subscription
 from core.text import AdminUsersHelpersText
 
 
