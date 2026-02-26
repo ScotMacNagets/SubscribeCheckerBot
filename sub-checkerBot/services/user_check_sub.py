a09a7901b7e7831cb729a14ca6a8829ed952d060
Mikhail Kopiistov <mihail1kopiistov@gmail.com>
2026-02-26 12:26:25 +0300

    Add additional check fields in user_check_sub.py

--- sub-checkerBot/services/user_check_sub.py
+++ sub-checkerBot/services/user_check_sub.py
@@ -3,14 +3,20 @@ import logging
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import User
+from core.models import User, Subscription
 
 logger = logging.getLogger(__name__)
 
 
 async def self_check(user_id, session: AsyncSession):
     try:
-        result = await session.execute(select(User.subscription_end).where(User.id == user_id))
+        result = await session.execute(
+            select(Subscription.expires_at)
+            .where(
+                Subscription.user_id == user_id,
+                Subscription.is_active == True,
+            )
+        )
         sub_end = result.scalars().one_or_none()
         return sub_end
     except Exception as db_error:
